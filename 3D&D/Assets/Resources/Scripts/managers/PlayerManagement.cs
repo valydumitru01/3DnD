3f5ea977b0414edacea7c8e12480694ee6ce9f64
UO275778 <crasarionic@gmail.com>
2023-01-18 19:35:53 +0100

    Message Victory

--- 3D&D/Assets/Resources/Scripts/managers/PlayerManagement.cs
+++ 3D&D/Assets/Resources/Scripts/managers/PlayerManagement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,24 +15,38 @@ public class PlayerManagement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var avatars = GameObject.FindGameObjectsWithTag("Avatar");
-        foreach(GameObject minion in avatars){
-            if(minion.GetComponent<MinionCharacter>().currentHealth <= 0){
-                StartCoroutine(FinishGame());
+        if(CheckGameIsOver)
+            StartCoroutine(FinishGame());
+    }
+
+    public void ChangePlayer(){
+        if(! CheckGameIsOver()){
+            if(activePlayer == 1){
+                activePlayer = 2;
+            }else{
+                activePlayer = 1;
             }
         }
     }
 
-    public void ChangePlayer(){
-        if(activePlayer == 1){
-            activePlayer = 2;
-        }else{
-            activePlayer = 1;
+    private bool CheckGameIsOver(){
+        var avatars = GameObject.FindGameObjectsWithTag("Avatar");
+        foreach(GameObject minion in avatars){
+            if(minion.GetComponent<MinionCharacter>().currentHealth <= 0){
+                return true;
+            }
         }
+        return false;
     }
 
     private IEnumerator FinishGame(){
         yield return new WaitForSeconds(10);
-        Application.Quit();
+        var victoryText = GameObject.FindGameObjectsWithTag("VictoryText");
+        victoryText.active = true;
+        if(activePlayer == 1){
+            victoryText.text = "Knight Warrior won";
+        }else{
+            victoryText.text = "Demonic Mage won";
+        }
     }
 }
